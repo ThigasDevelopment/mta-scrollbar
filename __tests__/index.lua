e903eab427b887efbc1c155b2d9dac41a530a3b5
ThigasDevelopment <maia.thico@gmail.com>
2025-08-27 18:29:03 -0300

    :recycle: Refactor example code.

--- __tests__/index.lua
+++ __tests__/index.lua
@@ -1,5 +1,5 @@
 local scrolls = {
-	Scrollbar.new (10, 150, 30, 75, 100, 85),
+	Scrollbar.new (10, 150, 30, 75, 100),
 }
 
 addEventHandler ('onClientRender', root,
@@ -12,30 +12,6 @@ addEventHandler ('onClientRender', root,
 
 			scroll:draw (x, 500, { default = tocolor (255, 0, 0, 255), background = tocolor (255, 255, 255, 255), effect = tocolor (0, 0, 255, 255) });
 		end
-
-		return true;
-	end
-);
-
-local stage = 0;
-bindKey ('arrow_u', 'down',
-	function ()
-		local scroll = scrolls[1];
-		if (not scroll) then
-			return false;
-		end
-
-		local values = {
-			85
-		};
-
-		stage = (stage + 1);
-		if (stage > #values) then
-			stage = 1;
-		end
-
-		local value = values[stage];
-		scroll:set (value);
 
 		return true;
 	end
